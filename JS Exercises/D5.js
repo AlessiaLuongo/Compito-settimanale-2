6962392aa4ca52efcc5425918823392a598f0a05
AlessiaLuongo <ale_yo@live.de>
2023-12-01 15:52:22 +0100

    esercizi fino al 10 fatti

--- JS Exercises/D5.js
+++ JS Exercises/D5.js
@@ -58,21 +58,56 @@ const cars = [
     trims: ["life", "style", "r-line"],
   },
 ];
+for (let i = 0; i < cars.length; i++) {
+  cars[i].licensePlate = cars[i].model + Math.floor(Math.random() * 1000);
+}
+
+console.log(cars);
 
 /* ESERCIZIO 6
     Scrivi del codice per aggiungere un nuovo oggetto in ultima posizione nell'array "cars", rispettando la struttura degli altri elementi.
     Successivamente, rimuovi l'ultimo elemento della proprietà "trims" da ogni auto.
 */
+const newCar = {
+  brand: "BMW",
+  model: "X5",
+  color: "black",
+  trims: ["life-style", "super", "something"],
+};
+cars.push(newCar);
+console.log(cars);
+
+for (let i = 0; i < cars.length; i++) {
+  cars[i].trims.pop();
+}
+
+console.log(cars);
 
 /* ESERCIZIO 7
     Scrivi del codice per salvare il primo elemento della proprietà "trims" di ogni auto nel nuovo array "justTrims", sotto definito.
 */
 const justTrims = [];
 
+for (let i = 0; i < cars.length; i++) {
+  let myTrim = cars[i].trims.splice(0, 1).join();
+  justTrims.push(myTrim);
+}
+console.log(justTrims);
+
 /* ESERCIZIO 8
     Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
     "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
 */
+
+for (let i = 0; i < cars.length; i++) {
+  let lettera = cars[i].color.charAt(0);
+
+  if (lettera === "b") {
+    console.log("Fizz");
+  } else {
+    console.log("Buzz");
+  }
+}
 
 /* ESERCIZIO 9
     Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
@@ -80,6 +115,12 @@ const justTrims = [];
 const numericArray = [
   6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
 ];
+let i = 0;
+while (numericArray[i] !== 32) {
+  console.log(numericArray[i]);
+
+  i++;
+}
 
 /* ESERCIZIO 10
     Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
